b4dabcd41ebad96852d02297dbea06742ba885eb
eliseev_aa <eliseev_aa@k12.ru>
2015-09-29 10:38:23 +0800

    +readme

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+require_dependency 'redmine_telegram_to_users/listener'
+
+Redmine::Plugin.register :redmine_telegram do
+	name 'Redmine Telegram To Users'
+	author 'com-k12'
+	url 'https://github.com/com-k12/redmine-telegram-to-users'
+	author_url 'http://k12.ru'
+	description 'Telegram integration'
+	version '1.0'
+end
---
+++ lib/redmine_slack/listener.rb
@@ -0,0 +1,117 @@
+require 'httpclient'
+require 'active_resource'
+
+class SlackListener < Redmine::Hook::Listener
+
+	def controller_issues_new_after_save(context={})
+
+		$stdout = File.open('f_controller_issues_new_after_save_telegram.txt', 'a')
+		$stderr = File.open('f_err_controller_issues_new_after_save_telegram.txt', 'a')
+
+    p context
+    return
+
+		issue = context[:issue]
+
+		journal = issue.current_journal
+		p "journal", journal
+
+		if journal != nil then
+
+			to = journal.notified_users
+			cc = journal.notified_watchers
+			watchers = to | cc
+			cu = User.current
+			if cu.pref.no_self_notified == true then
+				watchers.delete(cu)
+			end
+			for user in watchers
+				cv = User.find_by_mail(user[:mail]).custom_value_for(2)
+				next unless cv
+				slack_users.push(cv.value)
+			end
+
+		else
+
+			cu = User.current
+			recipients = issue.recipients
+			for mail in recipients
+				us = User.find_by_mail(mail)
+
+				if us == cu && cu.pref.no_self_notified == true then
+					next
+				end
+
+				cv = us.custom_value_for(2)
+				puts cv, cv.class
+				next unless cv
+				slack_users.push(cv.value)
+			end
+
+		end
+
+		p slack_users
+		slack_users.map{|user| (speak msg, user)}
+
+	end
+
+	def controller_issues_edit_after_save(context={})
+		$stdout = File.open('f_controller_issues_edit_after_save_telegram.txt', 'a')
+
+    p context
+    return
+
+		issue = context[:issue]
+		journal = context[:journal]
+
+		url = url_for_project issue.project
+		return unless url
+
+		msg = issue.project + journal.user.to_s + "updated" + issue
+
+		to = journal.notified_users
+    cc = journal.notified_watchers
+		watchers = to | cc
+		cu = User.current
+		if cu.pref.no_self_notified == true then
+			watchers.delete(cu)
+		end
+		slack_users = []
+		for user in watchers
+			cv = User.find_by_mail(user[:mail]).custom_value_for(2)
+			next unless cv
+			slack_users.push(cv.value)
+		end
+		p watchers, slack_users
+		slack_users.map{|user| (speak msg, user)}
+	end
+
+
+
+	def speak(msg, user)
+    $stdout = File.open('f_controller_speak_telegram.txt', 'a')
+
+    url = Setting.plugin_redmine_telegram[:telegram_url] if not url
+
+    f = File.new("../../source")
+    my_hash = JSON.parse(f.read)
+    f.close
+    user = my_hash[user]
+
+    p user
+
+    return unless user
+
+		params = {
+      :chat_id => user,
+			:text => msg
+		}
+
+    p params
+
+		client = HTTPClient.new
+		client.ssl_config.cert_store.set_default_paths
+		client.ssl_config.ssl_version = "SSLv23"
+		client.post url, {:payload => params.to_json}
+  end
+end
