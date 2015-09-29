bd830fb0aebbe6979e6eba98b329a369574a71be
eliseev_aa <eliseev_aa@k12.ru>
2015-09-29 15:14:19 +0800

    for tests

--- init.rb
+++ init.rb
@@ -8,6 +8,6 @@ Redmine::Plugin.register :redmine_telegram do
 	url 'https://github.com/com-k12/redmine-telegram-to-users'
 	author_url 'http://k12.ru'
 	description 'Telegram integration'
-	version '1.0'
+	version '0.1.0'
   settings :default => {'empty' => true}, :partial => 'settings/telegram_settings'
 end
